29ffe8107ccb339d6658b2fcea773cbff66b095e
berekettena <tena21bereket@gmail.com>
2024-05-20 11:18:04 +0300

    method to search and return all applicants that applied for specific job id added

--- models/sign_up.py
+++ models/sign_up.py
@@ -18,6 +18,10 @@ columns = [
 
 
 def insert_into_sign_up(data):
+    """
+    a function to which accepts applicant detail from html form
+    inserts into database.
+    """
     with engine.connect() as conn:
         query = text(
             "INSERT INTO sign_up (username, age, address, email, password) VALUES (:username, :age, :address, :email, :password)")
@@ -28,3 +32,26 @@ def insert_into_sign_up(data):
                          "email": data['email'],
                          "password": data['password']})
         conn.commit()
+
+
+def view_reg_applicant_by_job_id():
+    """
+    returns all applicants who applied for specific
+    job using job_id
+    """
+    with engine.connect() as con:
+        query = "select * from sign_up"
+        result = con.execute(text(query))
+        result_all = result.fetchall()
+
+        """
+        Convert each Row object into a dictionary and return dictionary
+        """
+
+        result_dicts = []
+        for row in result_all:
+            result_dict = {}
+            for column, value in zip(result.keys(), row):
+                result_dict[column] = value
+            result_dicts.append(result_dict)
+    return result_dicts
