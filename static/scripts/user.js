383651ba118b97c1da2a710b4734e32ae1f0c0b7
berekettena <tena21bereket@gmail.com>
2024-05-29 12:57:53 +0300

    email validation updated

--- static/scripts/user.js
+++ static/scripts/user.js
@@ -1,9 +1,10 @@
 function validateForm() {
-    var username = document.getElementById("username").value;
+    var email = document.getElementById("username").value;
     var password = document.getElementById("password").value;
-    if (username.trim() === "") {
-      alert("Username cannot be empty");
-      return false;
+    var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    if (!emailPattern.test(email)) {
+        alert("Please enter a valid email address.");
+        return false;
     }
     if (password.trim() === "") {
       alert("Password cannot be empty");
